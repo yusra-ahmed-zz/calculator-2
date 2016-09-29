7667a2a0c4383f9a6e04f6ee178c6ad0e7d4e7b0
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:33:50 -0700

    all calcs in

--- calculator.py
+++ calculator.py
@@ -25,4 +25,11 @@ while True:
         print square(float(maths[1]))
     if maths[0] == "cube":
         print cube(float(maths[1]))
-    
+    if maths[0] == "pow":
+        print power(float(maths[1]), float(maths[2]))
+    if maths[0] == "mod":
+        print mod(float(maths[1]), float(maths[2]))
+    if maths[0] == "q" or maths[0] == "quit":
+        print "Get out of here!"
+        break
+
